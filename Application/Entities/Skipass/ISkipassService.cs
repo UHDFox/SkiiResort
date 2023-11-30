1037b615344bc6a062f50a8ecaa483b97854a6a5
UHDFox <uhdfox@yandex.ru>
2023-11-30 12:07:08 +0300

    Removed .obj and .idea files

--- Application/Entities/Skipass/ISkipassService.cs
+++ Application/Entities/Skipass/ISkipassService.cs
@@ -1,15 +1,14 @@
 
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Application.Entities;
 
 public interface ISkipassService
 {
-    
     List<SkipassDto> ShowAllSkipasses();
     Task<SkipassDto> GetSkipassByID(int skipassId);
     Task<SkipassDto> AddNewSkipass(SkipassDto request);
     Task<SkipassDto> UpdateSkipassInfo(SkipassDto updatedSkipass);
     Task<SkipassDto> DeleteSkipass(int skipassId);
-
-
 }
