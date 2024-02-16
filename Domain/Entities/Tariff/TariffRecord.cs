56b6fb936bee858ea5711077bf20bf29d8eed2d4
UHDFox <uhdfox@yandex.ru>
2024-02-16 11:15:59 +0300

    Updated tariff entity & dto

--- Domain/Entities/Tariff/TariffRecord.cs
+++ Domain/Entities/Tariff/TariffRecord.cs
@@ -1,17 +1,26 @@
 using Domain.Entities.Skipass;
+using Domain.Entities.Tariffication;
 
 namespace Domain.Entities.Tariff;
 
 public sealed class TariffRecord
 {
-    public TariffRecord(string name)
+    public TariffRecord(string name, double priceModifier, bool isVip)
     {
         Name = name;
+        PriceModifier = priceModifier;
+        IsVip = isVip;
     }
 
     public Guid Id { get; set; }
 
     public string Name { get; set; }
+    
+    public double PriceModifier { get; set; }
+    
+    public bool IsVip { get; set; }
 
     public IReadOnlyList<SkipassRecord> Skipasses { get; set; } = new List<SkipassRecord>();
+    
+    public IReadOnlyList<TarifficationRecord> Tariffications { get; set; } = new List<TarifficationRecord>();
 }
