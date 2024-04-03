6375a69110ffa34cbe9a2bbcb3e141ac2c3674ef
UHDFox <uhdfox@yandex.ru>
2024-04-03 13:50:34 +0300

    Created extension methods class for Location

---
+++ Tests/Location/LocationExtensions.cs
@@ -0,0 +1,50 @@
+using Application.Location.Models;
+using Domain.Entities.Location;
+using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace Tests.Location;
+
+public static class LocationExtensions
+{
+    public static bool VerifyBy(this LocationRecord record, UpdateLocationModel model)
+    {
+        using (var scope = new AssertionScope())
+        {
+            record.Id.Should().Be(model.Id);
+            record.Name.Should().Be(model.Name);
+            
+            if (scope.HasFailures()) return false;
+        }
+
+        return true;
+    }
+    
+    public static bool VerifyBy(this LocationRecord record, AddLocationModel model)
+    {
+        using (var scope = new AssertionScope())
+        {
+            record.Name.Should().Be(model.Name);
+            
+            if (scope.HasFailures()) return false;
+        }
+
+        return true;
+    }
+    
+    
+    public static AddLocationModel ToAddModel(this LocationRecord record) => 
+        new AddLocationModel(record.Name);
+    
+    public static LocationRecord ToEntity(this AddLocationModel model) =>
+        new LocationRecord(model.Name);
+    
+    public static UpdateLocationModel ToUpdateModel(this LocationRecord record) => 
+        new UpdateLocationModel(record.Id, record.Name);
+
+    public static LocationRecord ToEntity(this UpdateLocationModel model) =>
+        new LocationRecord(model.Name);
+    
+    public static UpdateLocationModel ToGetModel(this LocationRecord record) => 
+        new UpdateLocationModel(record.Id, record.Name);
+}
