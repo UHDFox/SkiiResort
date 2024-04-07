b1f84449c7776025ba8c0d774e49e5eeeeb8fa45
UHDFox <uhdfox@yandex.ru>
2024-04-07 12:28:23 +0300

    Added method to get entity by LocationId and TariffId

--- Repository/Tariffication/ITarifficationRepository.cs
+++ Repository/Tariffication/ITarifficationRepository.cs
@@ -4,4 +4,5 @@ namespace Repository.Tariffication;
 
 public interface ITarifficationRepository : IRepository<TarifficationRecord>
 {
+    public Task<TarifficationRecord> GetByLocationAndTariffIdsAsync(Guid tariffId, Guid locationId);
 }
--- Repository/Tariffication/TarifficationRepository.cs
+++ Repository/Tariffication/TarifficationRepository.cs
@@ -6,22 +6,21 @@ namespace Repository.Tariffication;
 
 internal sealed class TarifficationRepository : ITarifficationRepository
 {
-    private readonly SkiiResortContext _context;
-
-
+    private readonly SkiiResortContext context;
+    
     public TarifficationRepository(SkiiResortContext context)
     {
-        this._context = context;
+        this.context = context;
     }
     
     public async Task<IReadOnlyCollection<TarifficationRecord>> GetListAsync(int offset, int limit)
     {
-        return await _context.Tariffications.Skip(offset).Take(limit).ToListAsync();
+        return await context.Tariffications.Skip(offset).Take(limit).ToListAsync();
     }
 
     public async Task<TarifficationRecord?> GetByIdAsync(Guid id)
     {
-        return await _context.Tariffications
+        return await context.Tariffications
             .AsNoTracking()
             .Include(x => x.Location)
             .Include(x => x.Tariff)
@@ -30,24 +29,33 @@ internal sealed class TarifficationRepository : ITarifficationRepository
 
     public async Task<Guid> AddAsync(TarifficationRecord data)
     {
-        var result = await _context.Tariffications.AddAsync(data);
+        var result = await context.Tariffications.AddAsync(data);
         await SaveChangesAsync();
         return result.Entity.Id;
     }
 
     public void UpdateAsync(TarifficationRecord data)
     {
-        _context.Tariffications.Update(data);
+        context.Tariffications.Update(data);
     }
 
     public async Task<bool> DeleteAsync(Guid id)
     {
-        _context.Tariffications.Remove((await GetByIdAsync(id))!);
+        context.Tariffications.Remove((await GetByIdAsync(id))!);
         return await SaveChangesAsync() > 0;
     }
 
     public async Task<int> SaveChangesAsync()
     {
-        return await _context.SaveChangesAsync();
+        return await context.SaveChangesAsync();
+    }
+
+    public async Task<TarifficationRecord> GetByLocationAndTariffIdsAsync(Guid tariffId, Guid locationId)
+    {
+        return await context.Tariffications
+            .AsNoTracking()
+            .Where(t => t.TariffId == tariffId)
+            .OrderByDescending(t => t.Price)
+            .FirstAsync(t => t.LocationId == locationId);
     }
 }
