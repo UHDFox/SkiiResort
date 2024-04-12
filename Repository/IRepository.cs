7bdd50c7fb3409c15f0eec0150aea97096a2e700
UHDFox <uhdfox@yandex.ru>
2024-04-12 14:14:29 +0300

    Added method to get total amount of record in db

--- Repository/IRepository.cs
+++ Repository/IRepository.cs
@@ -3,13 +3,15 @@ namespace Repository;
 public interface IRepository<TEntity>
 {
     Task<IReadOnlyCollection<TEntity>> GetListAsync(int offset, int limit);
-    
+
+    Task<int> GetTotalAmountAsync();
+
     Task<TEntity?> GetByIdAsync(Guid id);
-    
+
     Task<Guid> AddAsync(TEntity data);
-    
+
     void UpdateAsync(TEntity data);
-    
+
     Task<bool> DeleteAsync(Guid id);
 
     Task<int> SaveChangesAsync();
