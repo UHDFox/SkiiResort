0dac75a9f1139e25091e9f9710a60542a8b65f36
UHDFox <uhdfox@yandex.ru>
2024-04-12 14:26:43 +0300

    Added exception to handle wrong pagination queries

---
+++ Application/Exceptions/PaginationQueryException.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Exceptions;
+
+public sealed class PaginationQueryException : Exception
+{
+    public PaginationQueryException(string? message = default) : base(message)
+    {
+    }
+}
